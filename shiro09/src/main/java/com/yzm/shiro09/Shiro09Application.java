6fcdf80dafe94f9aad385244dd80bacbccd1cbec
yezhimincxvxb <395995486@qq.com>
2021-10-16 17:55:43 +0800

    shiro

---
+++ shiro09/src/main/java/com/yzm/shiro09/Shiro09Application.java
@@ -0,0 +1,13 @@
+package com.yzm.shiro09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shiro09Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shiro09Application.class, args);
+    }
+
+}
---
+++ shiro09/src/test/java/com/yzm/shiro09/Shiro09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzm.shiro09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shiro09ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
